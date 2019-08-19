e3fbfff1e39e587a34936243481a2a480e2ea12f
yaoxin <yaoxin@sunlands.com>
2019-08-19 14:43:52 +0800

    add

---
+++ Example/Pods/Target Support Files/core-lib2/core-lib2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_core_lib2 : NSObject
+@end
+@implementation PodsDummy_core_lib2
+@end
---
+++ core-lib2/Classes/Helper.swift
@@ -0,0 +1,12 @@
+//
+//  Helper.swift
+//  core-lib2
+//
+//  Created by y on 2019/8/19.
+//
+
+import UIKit
+
+class Helper: NSObject {
+
+}
